950c2cac402b9cf19f7023ee4a0d3920e0cfa601
Labh <kadulabhesh@gmail.com>
2026-09-14 20:41:02 +0530

    Configure production frontend CORS and WebSocket i.e WebConfig.java and WebSocketConfig.java

--- backend/src/main/java/com/tellmeko/quizapp/config/WebSocketConfig.java
+++ backend/src/main/java/com/tellmeko/quizapp/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.tellmeko.quizapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -12,10 +13,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${FRONTEND_URL:http://localhost:5173}")
+    private String frontendUrl;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:5173")
+                .setAllowedOriginPatterns(frontendUrl)
                 .withSockJS();
     }
 
